6e2eeb7c26cafb4f0f1f179c6113f16d66c7cbdd
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-03-01 01:17:25 +0600

    Update delete a  node.cpp

--- delete a  node.cpp
+++ delete a  node.cpp
@@ -1,87 +1,91 @@
-#include<stdio.h>
-#include<stdlib.h>
+/*
+Task 2:
+Extend your linked list operations. Implement functions to insert a node after a specific value
+or at a given position and to delete nodes with a given value or at a given position.
+
+Sample Input:
+- Insert 25 after 10.
+- Delete the node with the value 10.
+- Insert 20 at position 2.
+- Delete the node at position 3.
+Sample Output:
+Linked List: 5 -> 25 -> 20
+*/
+#include <bits/stdc++.h>
+using namespace std;
 struct Node
 {
     int data;
-    struct Node *next;
+    Node *next;
 };
-struct Node *head=NULL;
 
-//Fisrst Node Create
+Node *head = nullptr;
 void firstNode(int data)
 {
-    head=(struct Node*)malloc(sizeof(struct Node));
-    head->data=data;
-    head->next=NULL;
+    head = new Node;
+    head->data = data;
+    head->next = nullptr;
 }
-
-//Add nodes at end
 void addNode(int data)
 {
-    struct Node *ptr;
-    ptr=head;
-    struct Node *temp=NULL;
-    temp=(struct Node*)malloc(sizeof(struct Node));
-    temp->data=data;
-    temp->next=NULL;
-    while(ptr->next!=NULL)
+    Node *ptr;
+    ptr = head;
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = nullptr;
+    while (ptr->next != nullptr)
     {
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
-    ptr->next=temp;
+    ptr->next = temp;
 }
 
-
-//insert node at particular position
-void add_Node_n_position(int position,int data)
+void addNodeposition(int position, int data)
 {
-    struct Node *ptr=NULL;
-    struct Node *temp=NULL;
-    ptr=head;
-    temp=(struct Node*)malloc(sizeof(struct Node));
-    temp->data=data;
-    temp->next=NULL;
-    int i=0;
-    while(i!=position-1)
+    Node *ptr = head;
+    Node *temp = new Node;
+    temp->data = data;
+    temp->next = nullptr;
+    int i = 0;
+    while (i != position - 1)
     {
-        ptr=ptr->next;
+        ptr = ptr->next;
         i++;
     }
-    temp->next=ptr->next;
-    ptr->next=temp;
-
+    temp->next = ptr->next;
+    ptr->next = temp;
 }
 
-//delete Node at n position
-void del_n_position(int position)
+void deletePosition(int position)
 {
-    if(head->next==NULL)
+    if (head->next == nullptr)
     {
-        head=NULL;
+        head = nullptr;
     }
-    else{
-        struct Node *ptr;
-        ptr=head;
-        int i=0;
-        while(i!=position-1)
+    else
+    {
+        Node *ptr = head;
+        int i = 0;
+        while (i != position - 1)
         {
-            ptr=ptr->next;
+            ptr = ptr->next;
             i++;
         }
-        ptr->next=ptr->next->next;
+        ptr->next = ptr->next->next;
     }
 }
-
-//travers Node & print
 void printNode()
 {
-    struct Node *temp;
-    temp=head;
-    printf("Linked List: ");
-    while(temp!=NULL)
+    Node *temp = head;
+    cout << "Linked List: ";
+    while (temp != nullptr)
     {
-        printf("%d->",temp->data);
-        temp=temp->next;
+        cout << temp->data;
+        if (temp->next != nullptr)
+        {
+            cout << "->";
+        }
+        temp = temp->next;
     }
 }
 
@@ -91,14 +95,13 @@ int main()
     addNode(10);
     addNode(15);
     printNode();
-    printf("\n\n");
-    printf("After Extend Link: \n");
-    add_Node_n_position(2,25);
-    del_n_position(1);
-   add_Node_n_position(2,20);
-    del_n_position(3);
+    cout<<endl;
+    addNodeposition(2, 25);
+    deletePosition(1);
+    addNodeposition(2, 20);
+    deletePosition(3);
     printNode();
-    printf("\n\n\n");
+
 
     return 0;
 }
