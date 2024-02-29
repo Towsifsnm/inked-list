b02c8f627dd2f725abc15bb034b91c8333f17fbc
Towsif Mahabub <147875322+Towsifsnm@users.noreply.github.com>
2024-03-01 01:18:45 +0600

    Update basic singly.cpp

--- basic singly.cpp
+++ basic singly.cpp
@@ -1,55 +1,89 @@
-#include<stdio.h>
-#include<stdlib.h>
+
+/*Task 1:
+Create a basic singly linked list structure in C. Define a structure for a node that holds an
+integer value and a pointer to the next node. Implement functions to add nodes at the
+beginning and end of the list.
+
+Sample Input:
+- Add 5 to the beginning.
+- Add 10 to the end.
+- Add 15 to the end.
+
+Sample Output:
+Linked List: 5 -> 10 -> 15
+*/
+#include <bits/stdc++.h>
+using namespace std;
 struct Node
 {
     int data;
-    struct Node *next;
+    Node* next;
 };
-struct Node *head=NULL;
-
-//Fisrst Node Create
-void firstNode(int data)
+Node* createNode(int value)
 {
-    head=(struct Node*)malloc(sizeof(struct Node));
-    head->data=data;
-    head->next=NULL;
-}
-
-//Add nodes at end
-void addNode(int data)
-{
-    struct Node *ptr;
-    ptr=head;
-    struct Node *temp=NULL;
-    temp=(struct Node*)malloc(sizeof(struct Node));
-    temp->data=data;
-    temp->next=NULL;
-    while(ptr->next!=NULL)
+    Node* newNode = new Node;
+    if (newNode != nullptr)
     {
-        ptr=ptr->next;
+        newNode->data = value;
+        newNode->next = nullptr;
     }
-    ptr->next=temp;
+    return newNode;
 }
-
-//travers Node & print
-void printNode()
+Node* addBeginning(Node* head, int value)
 {
-    struct Node *temp;
-    temp=head;
-    printf("Linked List: ");
-    while(temp!=NULL)
+    Node* newNode = createNode(value);
+    if (newNode != nullptr)
     {
-        printf("%d->",temp->data);
-        temp=temp->next;
+        newNode->next = head;
+        head = newNode;
+    }
+    return head;
+}
+void addEnd(Node* head, int value)
+{
+    Node* newNode = createNode(value);
+    if (newNode != nullptr)
+    {
+        while (head->next != nullptr)
+        {
+            head = head->next;
+        }
+        head->next = newNode;
+    }
+}
+void printList(Node* head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data;
+        if (head->next != nullptr)
+        {
+            cout << " -> ";
+        }
+        head = head->next;
+    }
+    cout << endl;
+}
+void  deleteList(Node* head)
+{
+    Node* current = head;
+    while (current != nullptr)
+    {
+        Node* nextNode = current->next;
+        delete current;
+        current = nextNode;
     }
 }
 
 int main()
 {
-    firstNode(5);
-    addNode(10);
-    addNode(15);
-    printNode();
+    Node* head = nullptr;
+    head = addBeginning(head, 5);
+    addEnd(head, 10);
+    addEnd(head, 15);
+    cout << "Linked List: ";
+    printList(head);
 
+    deleteList(head);
     return 0;
 }
